31bd67837706f309a87c36d519b6f266f242569b
Joshua Davis <joshuad100717@outlook.com>
2024-09-12 08:39:07 -0600

    section 3

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,11 @@
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/App';
+import {App} from './components/App';
 
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
 );
-root.render(
-    <App />,
-);
+
 
